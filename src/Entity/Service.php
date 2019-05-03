56b037d52ad9a6ed7d9e771ef9f33b0c70272231
Akram HAMDI <akram.hamdi@sii.fr>
2019-05-03 12:20:28 +0200

    création des tables client et commande

---
+++ src/Entity/Service.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ServiceRepository")
+ */
+class Service
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lebelle;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLebelle(): ?string
+    {
+        return $this->lebelle;
+    }
+
+    public function setLebelle(string $lebelle): self
+    {
+        $this->lebelle = $lebelle;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
